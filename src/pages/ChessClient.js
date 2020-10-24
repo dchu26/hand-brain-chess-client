de75eaf2251be98df229a77318200a9399cef6b7
ynjoe <yuan.joe12@gmail.com>
2020-10-24 16:13:58 -0700

    Board square rendering

--- src/pages/ChessClient.js
+++ src/pages/ChessClient.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Chessboard from "chessboardjsx";
 import "../styles/ChessClient.css";
-import socket from "./socket";
+import socket from "../socket";
 
 class ChessClient extends React.Component {
   constructor(props) {
@@ -9,20 +9,31 @@ class ChessClient extends React.Component {
     this.state = {
       position: "start",
       currPiece: "",
-      isConnected: false
+      isConnected: false,
+      squareStyles: {}
     }
     this.onSquareClick = this.onSquareClick.bind(this);
     this.configureSocket();
   }
 
   configureSocket() {
-    socket.on("position", position => {
+    socket.emit("getBoard");
+    socket.on("board", boardState => {
       this.setState({
         isConnected: true,
-        position: position,
-        currPiece: ""
+        position: boardState.position,
+        currPiece: "",
+        squareStyles: this.getSquareStyles(boardState.squares)
       });
     });
+  }
+
+  getSquareStyles(squares) {
+    let squareStyles = {};
+    for (let square of squares) {
+      squareStyles[square] = {backgroundColor: "green"};
+    }
+    return squareStyles;
   }
 
   onSquareClick(square) {
@@ -37,10 +48,11 @@ class ChessClient extends React.Component {
     let board;
     if (this.state.isConnected) {
       board = <Chessboard
-      position={this.state.position}
-      draggable={false}
-      onSquareClick={this.onSquareClick}
-    />
+        position={this.state.position}
+        draggable={false}
+        onSquareClick={this.onSquareClick}
+        squareStyles={this.state.squareStyles}
+      />
     }
     return (
       <div className="ChessClient">
