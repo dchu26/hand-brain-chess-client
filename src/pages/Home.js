82f6cd8b2c455302fbceb359a763a58cb27cd9c0
ynjoe <yuan.joe12@gmail.com>
2020-10-30 02:13:54 -0700

    Home styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,11 @@ import {
   withRouter
 } from "react-router-dom";
 import socket from "../socket";
+import "../styles/Home.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class Home extends React.Component {
   constructor(props) {
@@ -18,7 +23,27 @@ class Home extends React.Component {
 
   render() {
     return (
-      <button className="play" onClick={this.createRoom}>Play</button>
+      <Container className="home vh-100 d-flex flex-column" fluid>
+        <Row className="h-25">
+          <Col>
+            <h1 className="title">Hand Brain Chess</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="rules">
+            <h2>Rules</h2>
+            <p>Each team contains 2 players: 1 hand and 1 brain.</p>
+            <p>Each turn, the brain makes a move using a piece. The type of piece they chose is highlighted yellow.</p>
+            <p>The team's hand must move a highlighted piece. Their move determines the actual move their side makes.</p>
+            <p>The hand and brain can't tell each other what move to make!</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button className="but" onClick={this.createRoom}>Play</Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
